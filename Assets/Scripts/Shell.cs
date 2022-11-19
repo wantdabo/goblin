4549f884f01273bd6b0afb25eb1cd93ca4feacb1
GoHo <icii8118@gmail.com>
2022-11-19 17:33:39 +0800

    分支移除战斗模块，重构

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -41,47 +41,4 @@ public class Shell : MonoBehaviour
     {
         Engine.TickEngine.FixedUpdate(Time.fixedDeltaTime);
     }
-
-#if UNITY_EDITOR
-    private void OnDrawGizmos()
-    {
-        DrawPhysicsCollisionLine();
-    }
-
-    private void DrawPhysicsCollisionLine()
-    {
-        if (null == Engine) return;
-        if (null == Engine.GameStage.State) return;
-        var gamePlayingState = Engine.GameStage.State as GoblinFramework.Client.GameStages.GamePlayingState;
-        if (null == gamePlayingState) return;
-
-        List<GoblinFramework.Gameplay.Physics.Comps.Collider> colliders = null;
-        gamePlayingState.PGEngine.World.GetColliders(ref colliders);
-
-        Gizmos.color = Color.yellow;
-        foreach (var collider in colliders)
-        {
-            if (collider is GoblinFramework.Gameplay.Physics.Comps.BoxCollider)
-            {
-                var boxCollider = collider as GoblinFramework.Gameplay.Physics.Comps.BoxCollider;
-                Gizmos.DrawWireCube(boxCollider.colliderPos.ToU3DVector3(), boxCollider.colliderScale.ToU3DVector3());
-            }
-            else if (collider is GoblinFramework.Gameplay.Physics.Comps.CylinderCollider)
-            {
-                var cylinderCollider = collider as GoblinFramework.Gameplay.Physics.Comps.CylinderCollider;
-                Gizmos.DrawWireCube(cylinderCollider.colliderPos.ToU3DVector3(), cylinderCollider.colliderScale.ToU3DVector3());
-            }
-            else if (collider is GoblinFramework.Gameplay.Physics.Comps.SphereCollider)
-            {
-                var sphereCollider = collider as GoblinFramework.Gameplay.Physics.Comps.SphereCollider;
-                Gizmos.DrawWireSphere(sphereCollider.colliderPos.ToU3DVector3(), sphereCollider.colliderScale.ToU3DVector3().x * 0.5f);
-            }
-            else if (collider is GoblinFramework.Gameplay.Physics.Comps.CapsuleCollider) 
-            {
-                var capsuleCollider = collider as GoblinFramework.Gameplay.Physics.Comps.CapsuleCollider;
-                Gizmos.DrawWireCube(capsuleCollider.colliderPos.ToU3DVector3(), capsuleCollider.colliderScale.ToU3DVector3());
-            }
-        }
-    }
-#endif
 }
