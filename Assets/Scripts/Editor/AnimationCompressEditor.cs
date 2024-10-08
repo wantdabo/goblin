b4299d42717cbabf46423d101833df7075ba2771
GoHo <wantdabo@gmail.com>
2024-10-08 10:59:19 +0800

    动画尺寸优化

---
+++ Assets/Scripts/Editor/AnimationCompressEditor.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class AnimationCompressEditor : Editor
+{
+    [MenuItem("工具/美术/压缩动画")]
+    public static void CompressAnimationClip()
+    {
+        var objs = Selection.objects;
+        if (null == objs) return;
+
+        foreach (var obj in objs)
+        {
+            if (obj is AnimationClip)
+            {
+                var clip = obj as AnimationClip;
+                AnimationClipCurveData[] curves = AnimationUtility.GetAllCurves(clip);
+                Keyframe key;
+                Keyframe[] keyFrames;
+                for (int ii = 0; ii < curves.Length; ++ii)
+                {
+                    AnimationClipCurveData curveDate = curves[ii];
+                    if (curveDate.curve == null || curveDate.curve.keys == null)
+                    {
+                        continue;
+                    }
+                    keyFrames = curveDate.curve.keys;
+                    for (int i = 0; i < keyFrames.Length; i++)
+                    {
+                        key = keyFrames[i];
+                        key.value = float.Parse(key.value.ToString("f3"));
+                        key.inTangent = float.Parse(key.inTangent.ToString("f3"));
+                        key.outTangent = float.Parse(key.outTangent.ToString("f3"));
+                        keyFrames[i] = key;
+                    }
+                    curveDate.curve.keys = keyFrames;
+                    clip.SetCurve(curveDate.path, curveDate.type, curveDate.propertyName, curveDate.curve);
+                }
+
+                // Save the modified clip
+                string path = AssetDatabase.GetAssetPath(clip);
+                AssetDatabase.CreateAsset(Object.Instantiate(clip), path);
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
