010a9b718e8629acd61beb21acc28b1e33ed7857
GoHo <wantdabo@gmail.com>
2024-10-10 18:54:18 +0800

    Update GameDef.cs

--- Assets/Scripts/Goblin/Gameplay/Common/Defines/GameDef.cs
+++ Assets/Scripts/Goblin/Gameplay/Common/Defines/GameDef.cs
@@ -8,7 +8,7 @@
         /// <summary>
         /// 逻辑帧率
         /// </summary>
-        public const byte LOGIC_FRAME = 5;
+        public const byte LOGIC_FRAME = 20;
         /// <summary>
         /// 逻辑帧 Tick
         /// </summary>
