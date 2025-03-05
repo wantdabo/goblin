7c1c73b7f5d492710c4840e628fda67229608399
GoHo <40389925+wantdabo@users.noreply.github.com>
2025-03-05 19:52:02 +0800

    Update KowtowExtension.cs

--- Assets/Scripts/Goblin/Gameplay/Logic/Common/Extensions/KowtowExtension.cs
+++ Assets/Scripts/Goblin/Gameplay/Logic/Common/Extensions/KowtowExtension.cs
@@ -7,10 +7,10 @@ namespace Goblin.Gameplay.Logic.Common.Extensions
     public static class KowtowExtension
     {
         /// <summary>
-        /// Vector3Data 转 FPVector
+        /// Vector3Data 转 FPVector3
         /// </summary>
         /// <param name="vectordata"></param>
-        /// <returns></returns>
+        /// <returns>FPVector3</returns>
         public static FPVector3 ToFPVector3(this Vector3Data vectordata)
         {
             return new FPVector3(vectordata.x * FP.EN3, vectordata.y* FP.EN3, vectordata.z* FP.EN3);
