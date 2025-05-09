7ab6cbbe4c45b33c36b0831b9a9132f150b5e33e
GoHo <40389925+wantdabo@users.noreply.github.com>
2025-05-09 14:48:34 +0800

    删除 BUFF 与 打断枚举

--- Assets/Scripts/Editor/Goblin.SkillpipelineEditor/Clips/Common/BuffSelfActivePopupData.cs
+++ Assets/Scripts/Editor/Goblin.SkillpipelineEditor/Clips/Common/BuffSelfActivePopupData.cs
@@ -7,9 +7,6 @@ namespace Goblin.SkillPipelineEditor
     {
         public override Dictionary<int, string> data => new()
         {
-            { BUFF_DEFINE.ACTIVE_SELF_NONE, "自身不生效" },
-            { BUFF_DEFINE.ACTIVE_SELF_TIMELINE, "自身时间轴生效" },
-            { BUFF_DEFINE.ACTIVE_SELF_HIT, "自身击中后生效" },
         };
     }
 }
--- Assets/Scripts/Editor/Goblin.SkillpipelineEditor/Clips/Common/BuffTargetActivePopupData.cs
+++ Assets/Scripts/Editor/Goblin.SkillpipelineEditor/Clips/Common/BuffTargetActivePopupData.cs
@@ -7,8 +7,6 @@ namespace Goblin.SkillPipelineEditor
     {
         public override Dictionary<int, string> data => new()
         {
-            { BUFF_DEFINE.ACTIVE_TARGET_NONE, "目标不生效" },
-            { BUFF_DEFINE.ACTIVE_TARGET_HIT, "目标击中后生效" },
         };
     }
 }
