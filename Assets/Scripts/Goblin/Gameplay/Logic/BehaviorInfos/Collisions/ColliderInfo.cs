902b25038686c51f9f236cefb6872b72fb076e69
GoHo <40389925+wantdabo@users.noreply.github.com>
2025-05-16 19:21:32 +0800

    碰撞盒信息留档

---
+++ Assets/Scripts/Goblin/Gameplay/Logic/BehaviorInfos/Collisions/ColliderInfo.cs
@@ -0,0 +1,24 @@
+using Goblin.Gameplay.Logic.Core;
+
+namespace Goblin.Gameplay.BehaviorInfos.Collisions
+{
+    public class ColliderInfo : BehaviorInfo
+    {
+        public byte shape { get; set; }
+        
+        protected override void OnReady()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnReset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override BehaviorInfo OnClone()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
