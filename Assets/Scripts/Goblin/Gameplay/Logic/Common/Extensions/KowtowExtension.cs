b8135dba319e91ad720ea0897bac82529973ba0f
GoHo <wantdabo@gmail.com>
2025-05-17 03:32:20 +0800

    删除无用文件

--- Assets/Scripts/Goblin/Gameplay/Logic/Common/Extensions/KowtowExtension.cs
+++ Assets/Scripts/Goblin/Gameplay/Logic/Common/Extensions/KowtowExtension.cs
@@ -1,4 +1,3 @@
-using Goblin.Gameplay.Common.SkillData.Common;
 using Goblin.Gameplay.Logic.Common.GPDatas;
 using Kowtow.Math;
 
@@ -6,16 +5,6 @@ namespace Goblin.Gameplay.Logic.Common.Extensions
 {
     public static class KowtowExtension
     {
-        /// <summary>
-        /// Vector3Data 转 FPVector3
-        /// </summary>
-        /// <param name="vector">三维向量数据</param>
-        /// <returns>FPVector3</returns>
-        public static FPVector3 ToFPVector3(this Vector3Data vector)
-        {
-            return new FPVector3(vector.x * FP.EN3, vector.y* FP.EN3, vector.z* FP.EN3);
-        }
-        
         /// <summary>
         /// GPVector3 转 FPVector3
         /// </summary>
