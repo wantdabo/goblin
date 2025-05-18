59601868bc7e79e2fd44c270cc2d5d6377c7974a
GoHo <wantdabo@gmail.com>
2025-05-18 22:53:57 +0800

    测试指令改为召唤单位

---
+++ Assets/Scripts/Goblin/Gameplay/Logic/BehaviorInfos/SkillCooldownInfo.cs
@@ -0,0 +1,25 @@
+using Goblin.Gameplay.Logic.Core;
+
+namespace Goblin.Gameplay.Logic.BehaviorInfos
+{
+    /// <summary>
+    /// 技能冷却信息
+    /// </summary>
+    public class SkillCooldownInfo : BehaviorInfo
+    {
+        protected override void OnReady()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnReset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override BehaviorInfo OnClone()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Assets/Scripts/Goblin/Gameplay/Logic/Flows/Executors/TestExecutor.cs
+++ Assets/Scripts/Goblin/Gameplay/Logic/Flows/Executors/TestExecutor.cs
@@ -1,6 +1,10 @@
 using Goblin.Gameplay.BehaviorInfos;
+using Goblin.Gameplay.Logic.BehaviorInfos;
 using Goblin.Gameplay.Logic.Flows.Executors.Common;
 using Goblin.Gameplay.Logic.Flows.Executors.Instructs;
+using Goblin.Gameplay.Logic.Prefabs;
+using Goblin.Gameplay.Logic.Prefabs.Datas;
+using Kowtow.Math;
 
 namespace Goblin.Gameplay.Logic.Flows.Executors
 {
@@ -11,6 +15,18 @@ namespace Goblin.Gameplay.Logic.Flows.Executors
     {
         protected override void OnEnter(TestInstr data, FlowInfo flowinfo)
         {
+            if (false == stage.SeekBehaviorInfo(flowinfo.owner, out SpatialInfo spatial)) return;
+            
+            stage.Spawn(new HeroPrefabInfo
+            {
+                hero = 100001,
+                spatial = new()
+                {
+                    position = spatial.position + FPVector3.forward,
+                    euler = spatial.euler,
+                    scale = spatial.scale,
+                }
+            });
         }
 
         protected override void OnExecute(TestInstr data, FlowInfo flowinfo)
