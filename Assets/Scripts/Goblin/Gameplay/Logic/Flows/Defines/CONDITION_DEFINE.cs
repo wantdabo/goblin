d0591dc70fb8271d868cbeb1d20943c8c020fc6e
GoHo <wantdabo@gmail.com>
2025-06-01 18:54:26 +0800

    Update CONDITION_DEFINE.cs

--- Assets/Scripts/Goblin/Gameplay/Logic/Flows/Defines/CONDITION_DEFINE.cs
+++ Assets/Scripts/Goblin/Gameplay/Logic/Flows/Defines/CONDITION_DEFINE.cs
@@ -6,6 +6,31 @@ namespace Goblin.Gameplay.Logic.Flows.Defines
     public class CONDITION_DEFINE
     {
         /// <summary>
+        /// 等于
+        /// </summary>
+        public const byte EQ = 0;
+        /// <summary>
+        /// 不等于
+        /// </summary>
+        public const byte NE = 1;
+        /// <summary>
+        /// 大于等于
+        /// </summary>
+        public const byte GE = 2;
+        /// <summary>
+        /// 小于等于
+        /// </summary>
+        public const byte LE = 3;
+        /// <summary>
+        /// 大于
+        /// </summary>
+        public const byte GT = 4;
+        /// <summary>
+        /// 小于
+        /// </summary>
+        public const byte LT = 5;
+        
+        /// <summary>
         /// 测试条件
         /// </summary>
         public const ushort TEST = 1;
