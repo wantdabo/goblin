c899def1d05f2155dfa570400b5e069a8f163dbc
GoHo <wantdabo@gmail.com>
2025-06-25 01:46:32 +0800

    Update BuffBucket.cs

--- Assets/Scripts/Goblin/Gameplay/Logic/Behaviors/Sa/BuffBucket.cs
+++ Assets/Scripts/Goblin/Gameplay/Logic/Behaviors/Sa/BuffBucket.cs
@@ -9,24 +9,53 @@ namespace Goblin.Gameplay.Logic.Behaviors.Sa
     {
         public void RmvBuff(ulong actor, uint buffid)
         {
+            if (false == stage.SeekBehaviorInfo(actor, out BuffBucketInfo buffbucket)) return;
+            if (false == buffbucket.buffdict.TryGetValue(buffid, out var buff)) return;
+            
+            buffbucket.buffdict.Remove(buffid);
+            buffbucket.buffs.Remove(buff);
+            stage.RmvActor(buff);
         }
 
         public void AddBuff(ulong actor, uint buffid, uint layer, ulong duration, List<uint> pipelines = null)
         {
             if (false == stage.SeekBehaviorInfo(actor, out BuffBucketInfo buffbucket)) buffbucket = stage.AddBehaviorInfo<BuffBucketInfo>(actor);
-            if (buffbucket.buffdict.TryGetValue(buffid, out var buff))
-            {
-                
-                return;
-            }
+            if (buffbucket.buffdict.ContainsKey(buffid)) return;
 
-            buff = stage.Spawn(new BuffPrefabInfo
+            var buff = stage.Spawn(new BuffPrefabInfo
             {
                 buffid = buffid,
                 layer = layer,
                 owner = actor,
                 pipelines = pipelines
             });
+            buffbucket.buffs.Add(buff);
+            buffbucket.buffdict.Add(buffid, buff);
+        }
+
+        public BuffInfo GetBuff(ulong actor, uint buffid)
+        {
+            if (false == stage.SeekBehaviorInfo(actor, out BuffBucketInfo buffbucket)) buffbucket = stage.AddBehaviorInfo<BuffBucketInfo>(actor);
+            if (false == buffbucket.buffdict.TryGetValue(buffid, out var buff) || false == stage.SeekBehaviorInfo(buff, out BuffInfo buffinfo)) return null;
+
+            return buffinfo;
+        }
+
+        public void AppBuff(ulong actor, uint buffid, uint layer, ulong duration)
+        {
+            var buffinfo = GetBuff(actor, buffid);
+            if (null == buffinfo) return;
+
+            buffinfo.layer += layer;
+            buffinfo.duration += duration;
+        }
+
+        public void SetBuff(ulong actor, uint buffid, uint layer, ulong duration)
+        {
+            if (false == stage.SeekBehaviorInfo(actor, out BuffBucketInfo buffbucket)) return;
+            if (false == buffbucket.buffdict.TryGetValue(buffid, out var buff) || false == stage.SeekBehaviorInfo(buff, out BuffInfo buffinfo)) return;
+            buffinfo.layer = layer;
+            buffinfo.duration = duration;
         }
     }
 }
