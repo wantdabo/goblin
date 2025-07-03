b1510b3358cc71ae8d2987c88cefa1d4c3d85ec0
GoHo <wantdabo@gmail.com>
2025-07-04 01:12:43 +0800

    ObjectCache 扩展 Capacity 创建临时提交

--- Assets/Scripts/Goblin/Gameplay/Logic/Common/ObjectCache.cs
+++ Assets/Scripts/Goblin/Gameplay/Logic/Common/ObjectCache.cs
@@ -14,6 +14,66 @@ namespace Goblin.Gameplay.Logic.Common
         /// 对象池字典，键为类型，值为该类型的对象池（字典，键为关键字，值为对象队列）
         /// </summary>
         private static readonly Dictionary<Type, Dictionary<string, Queue<object>>> pool = new();
+        private static readonly Dictionary<object, Queue<object>> capacityidentitys = new();
+        /// <summary>
+        /// 容量池字典，键为类型，值为该类型的容量池（字典，键为容量，值为对象队列）
+        /// </summary>
+        private static readonly Dictionary<Type, Dictionary<int, Queue<object>>> capacitypool = new();
+
+        public static T Ensure<T, TValue>(int capacity) where T : new()
+        {
+            var type = typeof(T);
+
+            if (false == capacitypool.TryGetValue(type, out var dict))
+            {
+                dict = new();
+                capacitypool.Add(type, dict);
+            }
+
+            if (false == dict.TryGetValue(capacity, out var queue))
+            {
+                queue = new();
+                dict.Add(capacity, queue);
+            }
+            
+            if (0 != queue.Count) 
+            {
+                if (queue.TryDequeue(out var obj)) return (T)obj;
+            }
+
+            T result = default;
+            if (type == typeof(List<TValue>))
+            {
+                result = (T)(new List<TValue>(capacity) as object);
+            }
+
+            if (type == typeof(HashSet<TValue>))
+            {
+                result = (T)(new HashSet<TValue>(capacity) as object);
+            }
+
+            if (type == typeof(Queue<TValue>))
+            {
+                result = (T)(new Queue<TValue>(capacity) as object);
+            }
+            
+            if (null == result) throw new InvalidOperationException($"unsupported type: {typeof(T)}");
+            
+            capacityidentitys.Add(result, queue);
+
+            return result;
+        }
+        
+        public static T Ensure<T, TKey, TValue>(int capacity) where T : new()
+        {
+            var type = typeof(T);
+            if (type == typeof(Dictionary<TKey, TValue>))
+            {
+                return (T)(new Dictionary<TKey, TValue>(capacity) as object);
+            }
+
+            throw new InvalidOperationException($"unsupported type: {typeof(T)}");
+        }
 
         /// <summary>
         /// 从对象池获得一个实例化对象
@@ -23,7 +83,13 @@ namespace Goblin.Gameplay.Logic.Common
         /// <returns>实例化对象</returns>
         public static T Ensure<T>(string key = "") where T : new()
         {
-            return (T)Ensure(typeof(T), key);
+            var type = typeof(T);
+            if (pool.TryGetValue(type, out var dict) && dict.TryGetValue(key, out var queue) && queue.Count > 0)
+            {
+                if (queue.TryDequeue(out var obj)) return (T)obj;
+            }
+            
+            return new T();
         }
         
         /// <summary>
@@ -51,6 +117,13 @@ namespace Goblin.Gameplay.Logic.Common
         {
             if (null == obj) return;
 
+            if (capacityidentitys.TryGetValue(obj, out var queue))
+            {
+                queue.Enqueue(obj);
+                
+                return;
+            }
+
             var type = obj.GetType();
             if (false == pool.TryGetValue(type, out var dict))
             {
@@ -58,7 +131,7 @@ namespace Goblin.Gameplay.Logic.Common
                 pool.Add(type, dict);
             }
 
-            if (false == dict.TryGetValue(key, out var queue))
+            if (false == dict.TryGetValue(key, out queue))
             {
                 queue = new();
                 dict.Add(key, queue);
