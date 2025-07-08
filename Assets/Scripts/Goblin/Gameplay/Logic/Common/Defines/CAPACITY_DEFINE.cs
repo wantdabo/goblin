4b47568c6837981d1dadbb2246a5fbbaf50984b2
GoHo <wantdabo@gmail.com>
2025-07-08 19:50:27 +0800

    ObjectCache 支持容器

---
+++ Assets/Scripts/Goblin/Gameplay/Logic/Common/Defines/CAPACITY_DEFINE.cs
@@ -0,0 +1,53 @@
+namespace Goblin.Gameplay.Logic.Common.Defines
+{
+    /// <summary>
+    /// 容量定义
+    /// </summary>
+    public class CAPACITY_DEFINE
+    {
+        /// <summary>
+        /// Level 0, 1024
+        /// </summary>
+        public const int L0 = 1 << 10;
+        /// <summary>
+        /// Level 1, 2048
+        /// </summary>
+        public const int L1 = 1 << 11;
+        /// <summary>
+        /// Level 2, 4096
+        /// </summary>
+        public const int L2 = 1 << 12;
+        /// <summary>
+        /// Level 3, 8192
+        /// </summary>
+        public const int L3 = 1 << 13;
+        /// <summary>
+        /// Level 4, 16384
+        /// </summary>
+        public const int L4 = 1 << 14;
+        /// <summary>
+        /// Level 5, 32768
+        /// </summary>
+        public const int L5 = 1 << 15;
+        /// <summary>
+        /// Level 6, 65536
+        /// </summary>
+        public const int L6 = 1 << 16;
+        /// <summary>
+        /// Level 7, 131072
+        /// </summary>
+        public const int L7 = 1 << 17;
+        /// <summary>
+        /// Level 8, 262144
+        /// </summary>
+        public const int L8 = 1 << 18;
+        /// <summary>
+        /// Level 9, 524288
+        /// </summary>
+        public const int L9 = 1 << 19;
+        /// <summary>
+        /// Level Full, 1048576
+        /// </summary>
+        public const int LF = 1 << 20;
+    }
+}
--- Assets/Scripts/Goblin/Gameplay/Logic/Common/ObjectCache.cs
+++ Assets/Scripts/Goblin/Gameplay/Logic/Common/ObjectCache.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using Goblin.Gameplay.Logic.Common.Defines;
 using Goblin.Gameplay.Logic.Core;
 
 namespace Goblin.Gameplay.Logic.Common
@@ -14,65 +15,82 @@ namespace Goblin.Gameplay.Logic.Common
         /// 对象池字典，键为类型，值为该类型的对象池（字典，键为关键字，值为对象队列）
         /// </summary>
         private static readonly Dictionary<Type, Dictionary<string, Queue<object>>> pool = new();
+        /// <summary>
+        /// 容量池字典，键为对象，值为该对象的容量队列
+        /// </summary>
         private static readonly Dictionary<object, Queue<object>> capacityidentitys = new();
         /// <summary>
         /// 容量池字典，键为类型，值为该类型的容量池（字典，键为容量，值为对象队列）
         /// </summary>
         private static readonly Dictionary<Type, Dictionary<int, Queue<object>>> capacitypool = new();
-
-        public static T Ensure<T, TValue>(int capacity) where T : new()
+        
+        /// <summary>
+        /// 查询容器对象池对应的队列
+        /// </summary>
+        /// <param name="capacity">容量</param>
+        /// <typeparam name="T">类型</typeparam>
+        /// <returns>容器队列</returns>
+        private static Queue<object> QueryCapacity<T>(int capacity) where T : new()
         {
             var type = typeof(T);
-
             if (false == capacitypool.TryGetValue(type, out var dict))
             {
-                dict = new();
+                dict = new(CAPACITY_DEFINE.L0);
                 capacitypool.Add(type, dict);
             }
-
             if (false == dict.TryGetValue(capacity, out var queue))
             {
-                queue = new();
+                queue = new(CAPACITY_DEFINE.L6);
                 dict.Add(capacity, queue);
             }
+
+            return queue;
+        }
+
+        /// <summary>
+        /// 从对象池中获取一个实例化容器，如果不存在则创建一个新的实例
+        /// </summary>
+        /// <param name="capacity">容量</param>
+        /// <typeparam name="T">类型</typeparam>
+        /// <typeparam name="TValue">值类型</typeparam>
+        /// <returns>容器</returns>
+        /// <exception cref="InvalidOperationException">传入的容器类型为不支持类型</exception>
+        public static T Ensure<T, TValue>(int capacity) where T : new()
+        {
+            var queue = QueryCapacity<T>(capacity);
+            if (0 != queue.Count) if (queue.TryDequeue(out var obj)) return (T)obj;
+            object ins = default;
+            var type = typeof(T);
+            if (type == typeof(List<TValue>)) ins = new List<TValue>(capacity);
+            if (type == typeof(HashSet<TValue>)) ins = new HashSet<TValue>(capacity);
+            if (type == typeof(Queue<TValue>)) ins = new Queue<TValue>(capacity);
+            if (null == ins) throw new InvalidOperationException($"unsupported type: {typeof(T)}");
             
-            if (0 != queue.Count) 
-            {
-                if (queue.TryDequeue(out var obj)) return (T)obj;
-            }
+            capacityidentitys.Add(ins, queue);
 
-            T result = default;
-            if (type == typeof(List<TValue>))
-            {
-                result = (T)(new List<TValue>(capacity) as object);
-            }
-
-            if (type == typeof(HashSet<TValue>))
-            {
-                result = (T)(new HashSet<TValue>(capacity) as object);
-            }
-
-            if (type == typeof(Queue<TValue>))
-            {
-                result = (T)(new Queue<TValue>(capacity) as object);
-            }
-            
-            if (null == result) throw new InvalidOperationException($"unsupported type: {typeof(T)}");
-            
-            capacityidentitys.Add(result, queue);
-
-            return result;
+            return (T)ins;
         }
         
+        /// <summary>
+        /// 从对象池中获取一个实例化容器，如果不存在则创建一个新的实例
+        /// </summary>
+        /// <param name="capacity">容量</param>
+        /// <typeparam name="T">类型</typeparam>
+        /// <typeparam name="TKey">键类型</typeparam>
+        /// <typeparam name="TValue">值类型</typeparam>
+        /// <returns>容器</returns>
+        /// <exception cref="InvalidOperationException">传入的容器类型为不支持类型</exception>
         public static T Ensure<T, TKey, TValue>(int capacity) where T : new()
         {
+            var queue = QueryCapacity<T>(capacity);
+            if (0 != queue.Count) if (queue.TryDequeue(out var obj)) return (T)obj;
+            object ins = default;
             var type = typeof(T);
-            if (type == typeof(Dictionary<TKey, TValue>))
-            {
-                return (T)(new Dictionary<TKey, TValue>(capacity) as object);
-            }
-
-            throw new InvalidOperationException($"unsupported type: {typeof(T)}");
+            if (type == typeof(Dictionary<TKey, TValue>)) ins = new Dictionary<TKey, TValue>(capacity);
+            if (null == ins) throw new InvalidOperationException($"unsupported type: {typeof(T)}");
+            capacityidentitys.Add(ins, queue);
+            
+            return (T)ins;
         }
 
         /// <summary>
@@ -127,13 +145,13 @@ namespace Goblin.Gameplay.Logic.Common
             var type = obj.GetType();
             if (false == pool.TryGetValue(type, out var dict))
             {
-                dict = new();
+                dict = new(CAPACITY_DEFINE.L0);
                 pool.Add(type, dict);
             }
 
             if (false == dict.TryGetValue(key, out queue))
             {
-                queue = new();
+                queue = new(CAPACITY_DEFINE.L6);
                 dict.Add(key, queue);
             }
             
